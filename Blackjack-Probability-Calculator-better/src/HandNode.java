03cceac25cbbdb5410878c5c3a6ebaaaaffdc1d1
ThomasCenta <centathomas@gmail.com>
2017-11-08 07:58:32 -0500

    Refactored VariableRankHand to HandNode

--- Blackjack-Probability-Calculator-better/src/HandNode.java
+++ Blackjack-Probability-Calculator-better/src/HandNode.java
@@ -1,4 +1,4 @@
-public class VariableRankHand extends MinimalHand {
+public class HandNode extends MinimalHand {
 
   /**
    * The probability of getting this hand. Updated during calculations.
@@ -10,7 +10,7 @@ public class VariableRankHand extends MinimalHand {
    * stand position, does not point to other hands. Has size 10 if this has hand
    * size < 2, 13 otherwise.
    */
-  private VariableRankHand[] nextHands;
+  private HandNode[] nextHands;
 
   /**
    * average money by splitting this hand (if possible) made on a bet of $1.
@@ -46,14 +46,14 @@ public class VariableRankHand extends MinimalHand {
   /**
    * Default constructor. Makes an empty hand. Sets all money made to -3
    */
-  public VariableRankHand() {
+  public HandNode() {
     super();
     this.moneyMadeIfDoubling = -3;
     this.moneyMadeIfHitting = -3;
     this.moneyMadeIfSplitting = -3;
     this.moneyMadeIfStaying = -3;
     this.mostMoneyMade = -3;
-    this.nextHands = new VariableRankHand[13];
+    this.nextHands = new HandNode[13];
     this.bestMove = null;
   }
 
@@ -66,7 +66,7 @@ public class VariableRankHand extends MinimalHand {
    *          true if only the card values and splitting value are to be copied
    *          (not the money stuff)
    */
-  public VariableRankHand(VariableRankHand otherHand, boolean cardsOnly) {
+  public HandNode(HandNode otherHand, boolean cardsOnly) {
     this.cardsInHand13 = new int[13];
     this.cardsInHand10 = new int[10];
     for (int i = 0; i < 13; i++) {
@@ -93,7 +93,7 @@ public class VariableRankHand extends MinimalHand {
       this.bestMove = null;
     }
 
-    this.nextHands = new VariableRankHand[13];
+    this.nextHands = new HandNode[13];
     this.numCards = otherHand.totalNumCards();
     this.rankSplitOn = otherHand.getRankSplitOn();
   }
@@ -102,7 +102,7 @@ public class VariableRankHand extends MinimalHand {
     return this.bestMove;
   }
 
-  public void setNextHand(VariableRankHand otherHand, int nextCardRank) {
+  public void setNextHand(HandNode otherHand, int nextCardRank) {
     assert nextCardRank >= 0 && nextCardRank <= 12 : "invalid card rank";
     if (nextCardRank > 9 && this.numCards >= 2) {
       this.nextHands[9] = otherHand;
@@ -111,7 +111,7 @@ public class VariableRankHand extends MinimalHand {
     }
   }
 
-  public VariableRankHand getNextHand(int nextCardRank) {
+  public HandNode getNextHand(int nextCardRank) {
     assert nextCardRank >= 0 && nextCardRank <= 12 : "invalid card rank";
     if (nextCardRank > 9 && this.numCards >= 2) {
       return this.nextHands[9];
